18d7dec4ab4ec8a872035f037573745c0a8cd713
pasiastazebra <vobis08gw@gmail.com>
2023-09-24 01:36:41 +0200

    added a few functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,3 +18,43 @@ const enemyStats = {
     blue: document.getElementById("enemy-stats-blue"),
     green: document.getElementById("enemy-stats-green")
 }
+
+//functions
+
+const updateTextField = ( text ) => {
+    gameTextField.innerHTML = text;
+}
+
+const updatePlayerStats = ( red, blue, green ) => {
+    playerStats.red.innerHTML = red;
+    playerStats.blue.innerHTML = blue;
+    playerStats.green.innerHTML = green;
+}
+
+const updateEnemyStats = ( red, blue, green ) => {
+    enemyStats.blue.innerHTML = blue;
+    enemyStats.red.innerHTML = red;
+    enemyStats.green.innerHTML = green;
+}
+
+const takeInputValue = () => {
+    return gameInputField.value;
+}
+
+const createArray = (input) => {
+    return input.split("");
+}
+
+const createString = (string, array) => {
+    const letter = array.shift();
+
+    string += letter;
+
+    return {newString: string, newArray: array};
+}
+
+gameInputField.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+        
+    }
+});
