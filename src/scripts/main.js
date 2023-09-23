9c5385d235e9534a650e38f3a4319afad851f2f7
pasiastazebra <vobis08gw@gmail.com>
2023-09-23 23:48:38 +0200

    js getters

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+//textfields
+
+const gameTextField = document.getElementById("game-textfield");
+const gameInputField = document.getElementById("game-input");
+
+const gameIcon = document.getElementById("game-icon");
+
+//stats
+
+const playerStats = {
+    red: document.getElementById("player-stats-red"),
+    blue: document.getElementById("player-stats-blue"),
+    green: document.getElementById("player-stats-green")
+}
+
+const enemyStats = {
+    red: document.getElementById("enemy-stats-red"),
+    blue: document.getElementById("enemy-stats-blue"),
+    green: document.getElementById("enemy-stats-green")
+}
